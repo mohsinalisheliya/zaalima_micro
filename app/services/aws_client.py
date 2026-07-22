66931c7a335983a858be93174fe02dba011b21ac
RAGHUVARSHAN.V.R <raghuvarshan452@gmail.com>
2026-07-22 21:45:17 +0530

    feat: integrate config into S3 client

--- app/services/aws_client.py
+++ app/services/aws_client.py
@@ -1,5 +1,10 @@
 import boto3
+from app.core.config import settings
 
 def get_s3_client():
-    # We will configure credentials on Day 3
-    return boto3.client('s3')
+    return boto3.client(
+        's3',
+        aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+        region_name=settings.AWS_REGION
+    )
