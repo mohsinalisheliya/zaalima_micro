21563b55c1e06fd8e3273815ad32cf6f4ba8e83e
RAGHUVARSHAN.V.R <raghuvarshan452@gmail.com>
2026-07-23 19:22:36 +0530

    feat: implement generate_presigned_url

--- app/services/aws_client.py
+++ app/services/aws_client.py
@@ -3,3 +3,16 @@ import boto3
 def get_s3_client():
     # We will configure credentials on Day 3
     return boto3.client('s3')
+
+def generate_presigned_url(object_name: str):
+    s3_client = get_s3_client()
+    try:
+        response = s3_client.generate_presigned_url(
+            'put_object',
+            Params={'Bucket': settings.AWS_BUCKET_NAME, 'Key': object_name},
+            ExpiresIn=settings.PRESIGNED_URL_EXPIRE_SECONDS
+        )
+        return response
+    except Exception as e:
+        print(f"Error generating url: {e}")
+        return None
