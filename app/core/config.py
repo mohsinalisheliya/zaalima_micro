3468cd86dd2d5ecd75c57c9590d9fa4b316007d5
Sidram <sidramchalageri7@gmail.com>
2026-07-24 18:48:18 +0530

    feat: add presigned URL expiration setting

--- app/core/config.py
+++ app/core/config.py
@@ -1,10 +1,16 @@
 from pydantic_settings import BaseSettings
-
 class Settings(BaseSettings):
     PROJECT_NAME: str = "Media Processing Service"
-    AWS_REGION: str = "us-east-1"
+    AWS_REGION: str = "us-east-1
+    AWS_ACCESS_KEY_ID: str = ""
+    AWS_SECRET_ACCESS_KEY: str = ""
+    AWS_BUCKET_NAME: str = "my-media-bucket"
 
     class Config:
         env_file = ".env"
-
 settings = Settings()
+class Settings(BaseSettings):
+    APP_NAME: str = "Zaalima"
+    AWS_REGION: str = "ap-south-1"
+
+    PRESIGNED_URL_EXPIRE_SECONDS: int = 3600git status
